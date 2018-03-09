0ee03ceca8ce00209ddbeccaccae6b5ae4bc91d1
1103-12 <1103-12@1103-12-PC>
2018-03-09 20:51:38 +0900

    중간저장 2018-03-09

--- src/total/controller/NavController.java
+++ src/total/controller/NavController.java
@@ -2,6 +2,8 @@ package total.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,27 +11,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import total.service.GreetService;
 import total.service.NavService;
 
 @Controller
 @RequestMapping("/join")
 public class NavController {
-@Autowired
-NavService navservice;
+	@Autowired
+	NavService navservice;
+	@Autowired
+	GreetService greetService;
 
+	@RequestMapping(method = RequestMethod.GET)
+	public String joinGetHandle(Model model) {
+		model.addAttribute("ment", greetService.make());
+		return "nav";
+	}
 
-
-
-@RequestMapping(method= RequestMethod.POST)
-public String Nav01Handle(Model model,
-		@RequestParam Map<String, String> param) {
-	
-	
-	
-	model.addAttribute("Member", param.get("id"));
-	
-	return "nav";
-}
-
+	@RequestMapping(method = RequestMethod.POST)
+	public String joinPostHandle(@RequestParam Map<String, String> param, Model model, HttpSession session) {
+		try {
+			boolean rst =navservice.insert(param);
+			if (rst) {
+				session.setAttribute("logon", param.get("id"));
+				return "redirect:/";
+			}
+			throw new Exception();
+		} catch (Exception e) {
+			model.addAttribute("err", "계정생성에서 문제가 있었습니다.");
+			e.printStackTrace();
+			return "join";
+		}
+	}
 
 }
