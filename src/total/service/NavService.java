2e81ee9c7f45a3003e570cb4f734a5687dcbb68c
1103-12 <1103-12@1103-12-PC>
2018-03-09 21:28:29 +0900

    중간저장

--- src/total/service/NavService.java
+++ src/total/service/NavService.java
@@ -4,11 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NavService {
-
+@Autowired
 SqlSessionTemplate template;
 
 	
